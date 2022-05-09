4cd849bffc78d2554fb60d2200a88a873dab5922
arbazalam01 <drarbazalam@gmail.com>
2022-05-09 16:58:36 +0530

    Fixes few bugs

--- index.js
+++ index.js
@@ -2,20 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import bodyParser from "body-parser";
-
+import "dotenv/config";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-const CONNECTION_URL =
-  "mongodb+srv://arbazalam:arbazalam@cluster0.0grgd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_URL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@cluster0.0grgd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const PORT = process.env.PORT || 8000;
 
 app.use("/posts", postRoutes);
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(8000, () => console.log("Server Running!!")))
+  .then(() => app.listen(PORT, () => console.log("Server Running!!")))
   .catch((err) => console.log(err));
